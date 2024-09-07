87a3fd588ba11e4d79a32523434edba156c63812
hslau44 <hslau44@gmail.com>
2024-09-07 20:27:25 +0100

    add function binding

--- main.cpp
+++ main.cpp
@@ -14,8 +14,20 @@ double subtract(double a, double b) {
    return c.subtract(a, b) ;
 }
 
+double times(double a, double b){
+   Calculator c;
+   return c.times(a, b) ;
+}
+
+double divide(double a, double b){
+   Calculator c;
+   return c.divide(a, b) ;
+}
+
 PYBIND11_MODULE(SumFunction, m) {
    m.doc() = "pybind11 example module";
    m.def("suming_a_b", &add, "This function adds two input numbers");
    m.def("subtracting_a_b", &subtract, "This function subtract the first input by the second input");
+   m.def("multiplying_a_b", &times, "This function multiply the first input by the second input");
+   m.def("dividing_a_b", &divide, "This function divide the first input by the second input");
 }
