19bc38d5206c477a779f1b5da7f96f3f75cd9846
hslau44 <hslau44@gmail.com>
2024-09-08 15:24:05 +0100

    add approx

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from textmathsolve import TextMathSolver
 import pytest
+from pytest import approx
 
 
 @pytest.mark.parametrize("inputs,expected", [
     ("1+1", 2.0),
     ("1.1-3.2", -2.1),
-    ("6/2", 3.0),
+    ("6.4/1.6", approx(4)),
 ])
 def test_TextMathSolver(inputs, expected):
     solver = TextMathSolver()
